7afa2d4b00bc9ea59b3ea32048f320da72c84e00
Ian Benedict A. Mabalot <ianbenedictmabalot@gmail.com>
2022-02-01 19:06:08 +0800

    secondary commit
    tested synchronous button

--- AsyncDemo1/AsyncDemo1/Form1.Designer.cs
+++ AsyncDemo1/AsyncDemo1/Form1.Designer.cs
@@ -29,13 +29,59 @@ namespace AsyncDemo1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.executeSync = new System.Windows.Forms.Button();
+            this.executeAsync = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // executeSync
+            // 
+            this.executeSync.Location = new System.Drawing.Point(90, 12);
+            this.executeSync.Name = "executeSync";
+            this.executeSync.Size = new System.Drawing.Size(319, 23);
+            this.executeSync.TabIndex = 0;
+            this.executeSync.Text = "Syncronous";
+            this.executeSync.UseVisualStyleBackColor = true;
+            this.executeSync.Click += new System.EventHandler(this.executeSync_Click);
+            // 
+            // executeAsync
+            // 
+            this.executeAsync.Location = new System.Drawing.Point(90, 41);
+            this.executeAsync.Name = "executeAsync";
+            this.executeAsync.Size = new System.Drawing.Size(319, 23);
+            this.executeAsync.TabIndex = 1;
+            this.executeAsync.Text = "Asyncronous";
+            this.executeAsync.UseVisualStyleBackColor = true;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(87, 77);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(37, 13);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(512, 248);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.executeAsync);
+            this.Controls.Add(this.executeSync);
+            this.Name = "Form1";
+            this.Text = "Async Demo 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button executeSync;
+        private System.Windows.Forms.Button executeAsync;
+        private System.Windows.Forms.Label lblResult;
     }
 }
 
--- AsyncDemo1/AsyncDemo1/Form1.cs
+++ AsyncDemo1/AsyncDemo1/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,62 @@ namespace AsyncDemo1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void executeSync_Click(object sender, EventArgs e)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            RunDownloadSync(); //run this method syncronously
+
+            watch.Stop();
+
+            var elapseMs = watch.ElapsedMilliseconds;
+
+            lblResult.Text += $"Total execution time: {elapseMs}";
+        }
+
+        private List<string> PrepData()
+        {
+            List<string> output = new List<string>();
+
+            lblResult.Text = "";
+
+            output.Add("https://www.yahoo.com");
+            output.Add("https://www.google.com");
+            output.Add("https://www.microsoft.com");
+            output.Add("https://www.cnn.com");
+            output.Add("https://www.codeproject.com");
+            output.Add("https://www.stackoverflow.com");
+
+            return output;
+        }
+
+        private void RunDownloadSync()
+        {
+            List<string> websites = PrepData();
+
+            foreach (string site in websites)
+            {
+                WebsiteDataModel results = DownloadWebsite(site);
+                ReportWebsiteInfo(results);
+            }
+        }
+
+        private WebsiteDataModel DownloadWebsite(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+
+            output.WebsiteUrl = websiteURL;
+            output.WebsiteData = client.DownloadString(websiteURL);
+
+            return output;
+        }
+
+        private void ReportWebsiteInfo(WebsiteDataModel data)
+        {
+            lblResult.Text += $"{data.WebsiteUrl} downloaded: {data.WebsiteData.Length} characters long. {Environment.NewLine}";
         }
     }
 }
